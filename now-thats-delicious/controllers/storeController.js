dba8b109bc0f5d650ab476dbf119e8e87be3c7be
Greg Vargas <greg.vargas@silvercar.com>
2019-07-28 16:43:56 -0500

    Added method to create new stores

--- now-thats-delicious/controllers/storeController.js
+++ now-thats-delicious/controllers/storeController.js
@@ -1,3 +1,6 @@
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
+
 exports.homePage = (req, res) => {
   res.render('index');
 };
@@ -6,6 +9,11 @@ exports.addStore = (req, res) => {
   res.render('editStore', { title: 'Add Store' });
 };
 
-exports.createStore = (req, res) => {
-  res.json(req.body);
+exports.createStore = async (req, res) => {
+  const store = new Store(req.body);
+
+  // fires off connection to MDB, save data, and come
+  // back with either the store itself or an error
+  await store.save();
+  res.redirect('/');
 };
